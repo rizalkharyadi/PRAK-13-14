e7e8f926f7985c1fe1c88d7755936675a1fb3c99
rizalkharyadi <Rizal083822286857@gmail.Com>
2019-12-20 17:30:54 +0700

    file rizal

---
+++ 5-Organisasi Program dengan Package/src/Program_manual/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Program_manual;
+
+/**
+ * Nama : RIZAL KARYADI
+ * NIM  : A2.1900156
+ * Kelas: TI-1C
+ */
+public class Main {
+    public static void main(String[] args){
+		System.out.print("nama ku adalah Dani, teman si fajar");
+	}
+}
---
+++ 5-Organisasi Program dengan Package/src/Program_manual/hello.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Program_manual;
+
+/**
+ * Nama : RIZAL KARYADI
+ * NIM  : A2.1900156
+ * Kelas: TI-1C
+ */
+public class hello {
+    public static void main(String[] args) {
+         System.out.println("hello world");
+    }
+}
